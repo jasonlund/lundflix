ec11983497bc8b89b8c3b915dc93c3f9a8026690
jasonlund <jasonlund@gmail.com>
2026-02-02 22:48:26 -0700

    Add Lundbergh toast styling

---
+++ resources/views/flux/toast/index.blade.php
@@ -0,0 +1,24 @@
+@blaze
+
+@props([
+    'position' => 'bottom end',
+])
+
+<ui-toast x-data x-on:toast-show.document="! $el.closest('ui-toast-group') && $el.showToast($event.detail)" popover="manual" position="{{ $position }}" wire:ignore>
+    <template>
+        <div {{ $attributes->only(['class'])->class('max-w-sm in-[ui-toast-group]:max-w-auto in-[ui-toast-group]:w-xs sm:in-[ui-toast-group]:w-sm') }} data-variant="" data-flux-toast-dialog>
+            <div class="p-2">
+                <x-lundbergh-bubble class="!mt-0">
+                    <span class="flex flex-col gap-1">
+                        <span class="text-sm font-semibold text-zinc-900 dark:text-zinc-100 empty:hidden">
+                            <slot name="heading"></slot>
+                        </span>
+                        <span class="text-sm text-zinc-600 dark:text-zinc-300">
+                            <slot name="text"></slot>
+                        </span>
+                    </span>
+                </x-lundbergh-bubble>
+            </div>
+        </div>
+    </template>
+</ui-toast>
